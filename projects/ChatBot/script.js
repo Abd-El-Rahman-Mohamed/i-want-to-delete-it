88690d880c757be9a3347d2a244734c78074549f
Abd-El-Rahman-Mohamed <90394194+Abd-El-Rahman-Mohamed@users.noreply.github.com>
2022-12-09 14:40:44 +0200

    Recreating the fetch request again

--- projects/ChatBot/script.js
+++ projects/ChatBot/script.js
@@ -6,10 +6,16 @@ let url = "";
 let ans = "";
 
 askBTN.addEventListener('click', () => {
+    console.log('btn is clicked');
     ask = theQ.value;
     url = "https://api.pawan.krd/chat/gpt?text=" + urlParser(ask) + "&lang=en/gpt.json";
 
-    
+    let myJSON;
+    fetch(url)
+        .then(response => response.json())
+        .then(data => myJSON = data)
+        .then(() => console.log(myJSON))
+        .catch(error => console.error(error));
 });
 
 // To make the question able to be sent
